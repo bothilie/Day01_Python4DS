439750c587c46a5a29820275f09bff81f5418695
bothilie <bothilie@42.student.fr>
2025-02-13 10:22:15 +0100

    day01 complete

--- ex05/pimp_image.py
+++ ex05/pimp_image.py
@@ -1,19 +1,98 @@
 import numpy as np
 
+#cas testé
+#-  arr null
+#- pas 3 pixels dans chaque array
+#- Pas 3 dimensions?
+
+
+
 def ft_invert(arr: []) -> []:
-    return arr
+    ft_invert.__doc__= "filter the image in the inverse colors"
+    try:
+        if arr == () or arr[0] == () or arr[1] == () :
+            return []
+    except:
+        M1=arr
+        n=len(M1)
+        p=len(M1[0])
+        for i in range(n):
+            for j in range(p):
+                #print(M1[i][j])
+                M1[i][j]= [255-M1[i][j][0], 255-M1[i][j][1], 255-M1[i][j][2]]
+        M1 = np.array(M1)
+        return M1 
+        return arr
 
 def ft_red(arr: []) -> []:
     ft_red.__doc__= "filter the image in red"
-    M1=arr
-    n=len(M1)
-    p=len(M1[0])
-    for i in range(n):
-        for j in range(p):
-            #print(M1[i][j])
-            M1[i][j]= M1[i][j]*[0.8, 0, 0]
-    M1 = np.array(M1)
-    return M1
+    try:
+        if arr == () or arr[0] == () or arr[1] == () :
+            return []
+    except:
+       # AssertionError("AssertionError")
+        M1=arr
+        n=len(M1)
+        p=len(M1[0])
+        for i in range(n):
+            for j in range(p):
+                if len(M1[i][j]) != 3:
+                    return[]
+                #print(M1[i][j])
+                M1[i][j]= M1[i][j]*[0.8, 0, 0]
+        M1 = np.array(M1)
+        return M1
+
+
+def ft_green(arr: []) -> []:
+    ft_green.__doc__= "filter the image in green"
+    try:
+        if arr == () or arr[0] == () or arr[1] == () :
+            return []
+    except:
+        M1=arr
+        n=len(M1)
+        p=len(M1[0])
+        for i in range(n):
+            for j in range(p):
+                M1[i][j]-= np.array([M1[i][j][0], 0, M1[i][j][2]], dtype='uint8')
+        M1 = np.array(M1)
+        return M1
+
+def ft_blue(arr: []) -> []:
+    ft_blue.__doc__= "filter the image in blue"
+    try:
+        if arr == () or arr[0] == () or arr[1] == () :
+            return []
+    except:
+        M1=arr
+        n=len(M1)
+        p=len(M1[0])
+        for i in range(n):
+            for j in range(p):
+                M1[i][j][0]= np.array(0)
+                M1[i][j][1]= np.array(0)
+        M1 = np.array(M1)
+        return M1
+
+def ft_grey(arr: []) -> []:
+    ft_grey.__doc__= "filter the image in grey"
+    try:
+        if arr == () or arr[0] == () or arr[1] == () :
+            return []
+    except:
+        M1=arr
+        n=len(M1)
+        p=len(M1[0])
+        M1 = M1[:,:,1]
+        #for i in range(n):
+        #    for j in range(p):
+        #        M1[i][j][0]= np.array(0)
+        #        M1[i][j][1]= np.array(0)
+        #        M1[i][j][2]= np.array(0)
+        M1 = np.array(M1)
+        return M1
+
 
 if __name__ == "__main__":
     main()
--- ex05/tester.py
+++ ex05/tester.py
@@ -1,14 +1,23 @@
 from load_image import ft_load
 import imageio as iio
 from pimp_image import ft_invert
-from pimp_image import ft_red
+from pimp_image import ft_red, ft_green, ft_blue, ft_grey
 
 
 array = ft_load("landscape.jpg")
-ft_invert(array)
+M1 = ft_invert(array)
+iio.imwrite("new_invert.jpeg", M1)
+array = ft_load("landscape1.jpg")
 M2 = ft_red(array)
-iio.imwrite("new.jpeg", M2)
-#ft_green(array)
-#ft_blue(array)
-#ft_grey(array)
+if M2:
+    iio.imwrite("new_red.jpeg", M2)
+array = ft_load("landscape.jpg")
+M3 = ft_green(array)
+iio.imwrite("new_green.jpeg", M3)
+array = ft_load("landscape.jpg")
+M4 = ft_blue(array)
+iio.imwrite("new_blue.jpeg", M4)
+array = ft_load("landscape.jpg")
+M5 = ft_grey(array)
+iio.imwrite("new_grey.jpeg", M5)
 print(ft_red.__doc__)
